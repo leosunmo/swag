d5b5c5c33293be122a252ca9776698fbd1094f4e
Leo Palmer <leps@boozt.com>
2023-03-11 22:00:36 +0100

    Find and replace module names to enable go install

--- cmd/swag/main.go
+++ cmd/swag/main.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	"github.com/swaggo/swag"
-	"github.com/swaggo/swag/format"
-	"github.com/swaggo/swag/gen"
+	"github.com/leosunmo/swag"
+	"github.com/leosunmo/swag/format"
+	"github.com/leosunmo/swag/gen"
 )
 
 const (
--- format/format.go
+++ format/format.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/swaggo/swag"
+	"github.com/leosunmo/swag"
 )
 
 // Format implements `fmt` command for formatting swag comments in Go source
